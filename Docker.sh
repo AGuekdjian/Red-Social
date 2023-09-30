57381e2f933a4a8cc10cfc03093854afbe9b9879
Anthony Guekdjian <anthonyguekdjian@gmail.com>
2023-09-30 15:22:23 -0300

    Corrigiendo puertos y arreglando configuracion de docker compose

--- Docker.sh
+++ Docker.sh
@@ -6,15 +6,6 @@ check_exit_code() {
     exit 1
   fi
 }
-
-echo "***** Creando imagen de Frontend *****"
-cd ./frontend && sudo docker build -t frontend:2.0 . && cd ..
-check_exit_code "cd ./frontend && docker build -t frontend:1.0 . && cd .."
-
-echo "***** Creando imagen de Backend *****"
-cd ./backend && sudo docker build -t backend:2.0 . && cd ..
-check_exit_code "cd ./backend && docker build -t backend:1.0 . && cd .."
-
-echo "***** Creando y levantando contenedor *****"
+echo "***** Creando contenedor y levantando aplicacion *****"
 sudo docker compose up -d --build
-check_exit_code "docker-compose up -d --build"
+check_exit_code "sudo docker compose up -d --build"
