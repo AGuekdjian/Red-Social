46aec0be1fa30c36ad679e8720ec55d3b69bb14f
Anthony Guekdjian <anthonyguekdjian@gmail.com>
2023-09-30 13:27:24 -0300

    Agregando script para la instalacion de docker

--- Docker.sh
+++ Docker.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-cd ./frontend && docker build -t frontend:latest .
+check_exit_code() {
+  if [ $? -ne 0 ]; then
+    echo "Error en el comando: $1"
+    exit 1
+  fi
+}
 
-cd .. && cd ./backend && docker build -t backend:latest .
+echo "***** Creando imagen de Frontend *****"
+cd ./frontend && docker build -t frontend:1.0 . && cd ..
+check_exit_code "cd ./frontend && docker build -t frontend:1.0 . && cd .."
 
-cd .. && docker-compose up -d --build
+echo "***** Creando imagen de Backend *****"
+cd ./backend && docker build -t backend:1.0 . && cd ..
+check_exit_code "cd ./backend && docker build -t backend:1.0 . && cd .."
+
+echo "***** Creando y levantando contenedor *****"
+docker-compose up -d --build
+check_exit_code "docker-compose up -d --build"
